907f0f1fd7480f9dadf37a08b90b39cd136e076a
Vladyslav Kryklyvets <95692328+Vladyslav9112@users.noreply.github.com>
2026-03-24 10:46:21 +0200

    Updated AppLayout.tsx to replace h-svh with h-full

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import { BottomNav } from "./BottomNav";
 
 const AppLayout: React.FC = () => (
-  <div className="relative flex flex-col h-svh overflow-hidden bg-transparent">
+  <div className="relative flex flex-col h-full overflow-hidden bg-transparent">
     {/* Page content */}
     <main className="flex-1 overflow-y-auto overscroll-y-contain pb-[72px]">
       <Outlet />
