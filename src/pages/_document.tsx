b1ade5d28f49b6bb6b78b00132bd5189ec8509da
Vladyslav <vlad.kryklyvets9@gmail.com>
2026-03-29 15:18:46 +0300

    fix bugs in scroll

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover, interactive-widget=resizes-content"
         />
         <meta name="theme-color" content="#090910" />
         <meta name="color-scheme" content="dark" />
